400ddd58446f3dc7d0048fb19a50afa1debadc6a
Felipe Santos <felipe.santos@qualyteam.com.br>
2025-04-18 10:09:43 -0300

    Add: Colors tokens to Storybook

--- node_modules/@ignite-ui/docs/.storybook/main.ts
+++ node_modules/@ignite-ui/docs/.storybook/main.ts
@@ -1,30 +1,33 @@
 import type { StorybookConfig } from '@storybook/react-vite';
 import { createRequire } from 'module';
-import { join, dirname } from "path"
-
-/**
-* This function is used to resolve the absolute path of a package.
-* It is needed in projects that use Yarn PnP or are set up within a monorepo.
-*/
+import { join, dirname } from 'path';
 
 const require = createRequire(import.meta.url);
 
 function getAbsolutePath(value: string): string {
-  return dirname(require.resolve(join(value, 'package.json')))
+  return dirname(require.resolve(join(value, 'package.json')));
 }
+
 const config: StorybookConfig = {
-  "stories": [
-    "../src/**/*.mdx",
-    "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"
+  stories: [
+    '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)',
+    '../src/pages/**/*.stories.@mdx',
   ],
-  "addons": [
+  addons: [
+    {
+      name: getAbsolutePath('@storybook/addon-docs'),
+      options: {
+        mdxPluginOptions: {},
+        transcludeMarkdown: true,
+      },
+    },
     getAbsolutePath('@storybook/addon-essentials'),
-    getAbsolutePath('@storybook/addon-onboarding'),
-    getAbsolutePath('@chromatic-com/storybook')
+    getAbsolutePath('@chromatic-com/storybook'),
   ],
-  "framework": {
-    "name": getAbsolutePath('@storybook/react-vite'),
-    "options": {}
-  }
+  framework: {
+    name: getAbsolutePath('@storybook/react-vite'),
+    options: {},
+  },
 };
+
 export default config;
